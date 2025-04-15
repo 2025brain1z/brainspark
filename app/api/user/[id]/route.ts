8a9328230a6fb10cf2847000117b2b6ebfafebe4
2025brain1z <brainspark456@gmail.com>
2025-04-15 16:21:05 +0000

    Update route.ts deployment error stage 4

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -2,17 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/mongodb";
 import User from "@/models/User";
 
-type Context = {
-  params: {
-    id: string;
-  };
-};
-
-export async function GET(req: NextRequest, context: Context) {
+export async function GET(req: NextRequest, { params }: any) {
   await connectToDatabase();
-  const user = await User.findOne({ clerkId: context.params.id });
+  const user = await User.findOne({ clerkId: params.id });
 
-  if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
 
   return NextResponse.json(user);
 }
+
